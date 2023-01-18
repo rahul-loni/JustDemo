8990f9f03b8eaf517c67b79cf1b22c0e238ff5c6
Rahul Kumar <rahulloni000@gmail.com>
2023-01-19 00:11:08 +0545

    row

--- lib/main.dart
+++ lib/main.dart
@@ -12,38 +12,31 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("My App")),
-        body: Center(
-          child: Container(
-            // padding: const EdgeInsets.all(10),
-            // alignment: Alignment.center,
-
-            height: 160,
-            width: 160,
-            decoration: BoxDecoration(
-                // shape: BoxShape.circle,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 5,
-                      //spreadRadius: 10,
-                      offset: Offset(2.0, 5.0))
-                ],
-                color: Colors.green,
-                gradient: LinearGradient(colors: [Colors.red, Colors.yellow])),
-
-            child: Center(
-                child: Text(
-              "I am Box",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            )),
+      appBar: AppBar(title: Text("My App")),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            height: 100,
+            width: 100,
+            color: Colors.red,
           ),
-        ));
+          Container(
+            padding: const EdgeInsets.all(8),
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            height: 100,
+            width: 100,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+    );
   }
 }
